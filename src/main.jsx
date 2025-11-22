71797c65a123c619037c6a22a170af68a92286c1
Bunty <bn.bhandari90@gmail.com>
2025-11-22 17:35:56 +0530

    Add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/next"
 import { Buffer } from 'buffer'
 import './index.css'
 import App from './App.jsx'
@@ -11,5 +12,6 @@ globalThis.Buffer = Buffer
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
